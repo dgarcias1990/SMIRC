e6f7335c80629a017ada03b10f1df10629bab4f5
Diana Saldaña <dianasaldana@MacBook-Pro-de-Diana-Saldana.local>
2016-05-10 01:40:08 -0500

    Cambios realizados

--- sismoTT/settings.py
+++ sismoTT/settings.py
@@ -138,7 +138,7 @@ ALLOWED_HOSTS = ['*']
 
 import dj_database_url
 
-DATABASES=['default']=dj_database_url.config()
+DATABASES['default']=dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER={'HTTP_X_FORWARDED_PROTO','https'}
 
