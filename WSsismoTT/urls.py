3234e7787ef71aa8da33fc145042e41c7f621c2c
Diana Saldaña <dianasaldana@MacBook-Pro-de-Diana-Saldana.local>
2016-05-29 03:24:29 -0500

    uno

--- WSsismoTT/urls.py
+++ WSsismoTT/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls import include, url
-from .views import wsLogin, wsLocationsRegister,wsLogout
+from WSsismoTT.views import wsLogin, wsLocationsRegister,wsLogout
 
 urlpatterns = [
     url(r'^wsLogin/$', wsLogin,name='wsLogin'),
